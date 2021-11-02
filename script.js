472de545da060a26d36008798ad9cf65f701cbd4
Joseph Wallek <jwallek@gmail.com>
2021-11-01 21:02:18 -0700

    Pushing a completed version of the assignment

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    var final = document.getElementById("report");
+    var a = document.getElementById("deposit");
+    var b = document.getElementById("interestRate");
+    var c = document.getElementById("finalresult");
+    var d = document.getElementById("year");
+    var time = new Date().getFullYear();
     
+    time = time + +t;
+    let interest = ((p*r*t) / 100) + +p;
+   
+    a.innerHTML = " " + p;
+    b.innerHTML = " " + r;
+    c.innerHTML = " " + interest;
+    d.innerHTML = " " + time;  
+    
+    final.style.visibility ="visible";
 }
-        
